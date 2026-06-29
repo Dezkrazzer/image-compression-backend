70dd534d177188229ffe880b5b26d950e3464ab1
Dezkrazzer <uqie.vanmadiun@gmail.com>
2026-06-29 22:11:49 +0700

    config(Main): Change allowed origins to all

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ allowed_origins = [
 # Konfigurasi CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
